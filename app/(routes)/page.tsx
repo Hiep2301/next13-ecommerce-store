76e600622b369928226d29ff6679354ea3cab544
malino <adatx100@gmail.com>
2023-07-01 00:28:01 +0700

    up code

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,28 @@
+import getBillboard from '@/actions/get-billboard';
+import getProducts from '@/actions/get-products';
+import ProductList from '@/components/product-list';
+import Billboard from '@/components/ui/billboard';
+import Container from '@/components/ui/container';
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard('a026e11c-901e-4e48-9d1b-50568191640f');
+
+  return (
+    <Container>
+      <div className="pb-10 space-y-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col px-4 gap-y-8 sm:px-6 lg:px-8">
+          <ProductList
+            title="Featured Products"
+            items={products}
+          />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
