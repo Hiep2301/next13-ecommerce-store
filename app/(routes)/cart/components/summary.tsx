0b1fabc09add660ce1b7dfb7d4a66ed34e46cd92
malino <adatx100@gmail.com>
2023-07-06 11:44:38 +0700

    up

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"; 
 
 import axios from "axios";
 import { useEffect } from "react";
@@ -39,13 +39,13 @@ const Summary = () => {
 
   return ( 
     <div
-      className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8"
+      className="px-4 py-6 mt-16 rounded-lg bg-gray-50 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8"
     >
       <h2 className="text-lg font-medium text-gray-900">
         Order summary
       </h2>
       <div className="mt-6 space-y-4">
-        <div className="flex items-center justify-between border-t border-gray-200 pt-4">
+        <div className="flex items-center justify-between pt-4 border-t border-gray-200">
           <div className="text-base font-medium text-gray-900">Order total</div>
          <Currency value={totalPrice} />
         </div>
